dd51adf5580481497d6ad9aa0dce23551ef88cd6
dzm <1099279097@qq.com>
2023-12-28 15:15:35 +0800

    init

---
+++ page/index.vue
@@ -0,0 +1,33 @@
+<template>
+ <div class='page_wrap'>
+  测试
+ </div>
+</template>
+
+<script>
+ export default {
+  name: 'page',
+  components: { },
+  props: { },
+  data() {
+    return {};
+  },
+  computed: { },
+  watch: { },
+  created() { },
+  mounted() { },
+  methods: { },
+ }
+</script>
+
+<style lang='scss' scoped>
+
+.page_wrap {
+  z-index: inherit;
+  position: static;
+  width: auto;
+  height: auto;
+  font-size: inherit;
+  color: inherit;
+}
+</style>
